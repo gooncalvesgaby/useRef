85aefc2bc506052b2f822a261d5c5d8d57c35af5
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-18 13:48:15 -0300

    inclusão de tags

--- src/Components/Ref/ref.jsx
+++ src/Components/Ref/ref.jsx
@@ -3,7 +3,11 @@ import * as S from "./refStyle.jsx"
 function myRef() {
     return(
      <S.Container>
-        olá
+        <S.Form>
+            <label>Digite seu nome</label>
+            <input type="text" />
+            <button>salvar</button>
+        </S.Form>
      </S.Container>
     )
 }
