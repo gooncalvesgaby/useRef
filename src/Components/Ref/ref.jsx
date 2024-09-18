f4c266915873ba8d809be35ccc2253ae05a8c004
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-09-18 14:15:30 -0300

    useRef

--- src/Components/Ref/ref.jsx
+++ src/Components/Ref/ref.jsx
@@ -1,11 +1,21 @@
 import * as S from "./refStyle.jsx"
+import { useEffect, useRef } from "react"
 
 function myRef() {
+
+    const labelRef = useRef('')
+
+    useEffect(() => {
+        labelRef.current.innerHTML = "Digite o seu nome"
+    }, [])
+
+    //O useRef permite que eu manipule elementos diretamente no DOM
+    
     return(
      <S.Container>
         <S.Form>
-            <label>Digite seu nome</label>
-            <input type="text" />
+            <label ref={labelRef}>Nome</label>
+            <input placeholder="nome" type="text" />
             <button>salvar</button>
         </S.Form>
      </S.Container>
